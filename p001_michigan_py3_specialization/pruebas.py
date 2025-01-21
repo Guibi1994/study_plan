56c531d10b55c37ac5c58b7f370e7cf6953d687c
Guibor AGATA <guibor.camargo@urosario.edu.co>
2025-01-20 19:12:13 -0500

    pruebas

--- p001_michigan_py3_specialization/pruebas.py
+++ p001_michigan_py3_specialization/pruebas.py
@@ -1,6 +1,19 @@
-1 + 1
-
 import requests
 
-url = requests.get("https://github.com/Guibi1994")
-print(url.text.count(""))
+
+def get_joke_data(word: str) -> dict:
+    params = {"term": word, "limit": 2}
+    headers = {"Accept": "application/json"}
+    jokes = requests.get(
+        "https://icanhazdadjoke.com/search", params=params, headers=headers
+    )
+    print(jokes)
+    jokes = jokes.json()
+    return jokes
+    # return {"results": [x["joke"] for x in jokes["results"]]}
+
+
+result = get_joke_data("magic")
+
+type(result)
+len(result["results"])
