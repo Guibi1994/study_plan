c7ab0789f412af4b02e5db3f2dde9c28f766a4b0
Guibi1994 <guibor.camargo@urosario.edu.co>
2025-02-13 07:12:25 -0500

    Notas

--- p002_michigan_applied_ds_py_specialization/pruebas.py
+++ p002_michigan_applied_ds_py_specialization/pruebas.py
@@ -1,63 +1,24 @@
-# 0. Ejercicios prácticos
-
-alist = [1, 2, 4, 8, 16, 32, 64]
-
-## 0.1. Ejercicios con MAP:
-
-### 0.1.1. Devuelve el valor más "a"
-# Map no edita el valor original de la lista
-list(map(lambda x: str(x) + "a", alist))
-
-### 0.1.2. Devuleve el indice del valor dentro de una lista
-
-
-def return_letter(x: int) -> str:
-    letters = "abcdef"
-    try:
-        return letters[x]
-    except IndexError:
-        return "sin letra"
-
-
-list(map(return_letter, alist))
-
-### 0.1.3. Devuelve la misma aleatoriamente negativa o positivamente
-from random import choice
-
-list(map(lambda x: x * choice([-1, 1]), alist))
-
-### 0.1.4. Convierte cada elemento en una lista junto a su valo cuadrático
-list(map(lambda x: [x, x**2], alist))
-
-### 0.1.5. Cada objeto en un diccionario con su valor negativo aleatori y su cuadrado y raiz
-from math import sqrt
-
-list(
-    map(
-        lambda x: {
-            "valor": x * choice([-1, 1]),
-            "cuadrado": x**2,
-            "cuadratico medio": sqrt(x**2),
-        },
-        alist,
-    )
-)
-
-## 0.2. Ejercicios con list comprehension
-1 + 1
-
-
-# 1.
 import numpy as np
 
-data = np.array(
-    [
-        # Variabl 1
-        [1, 2, 3],
-        # Variable 2
-        [2, "", 2],
-    ]
-)
 
-data[:, 0] = 2
-data
+np.random.rand(2, 2, 2, 2)
+
+x = np.array([[1, 1, 1], [1, 1, 1]])
+y = x
+y[0, :2] = 0
+x
+
+x = [0, 1, 2, 3, 4, 5, 6]
+x[2:3]
+
+a1 = np.random.rand(4)
+a2 = np.random.rand(4, 1)
+a3 = np.array([[1, 2, 3, 4]])
+a4 = np.arange(1, 4, 1)
+a5 = np.linspace(1, 4, 4)
+
+a4.ndim() == 1
+a1.shape == a2.shape
+a5.shape == a1.shape
+
+np.arange(0, 36, 1).reshape(6, 6)[2:4, 2:4]
